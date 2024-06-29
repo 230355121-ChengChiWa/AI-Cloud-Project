8ecb8a37cf1678b0e15635c6b8ad5c74817419fb
MoRan <ooklibaioo@gmail.com>
2024-06-29 16:52:26 +0800

    python

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask as fla, request, jsonify
+from flask import render_template
+import boto3
+
+app=fla(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get_data', methods=['GET'])
+def get_data():
+    client = boto3.client('forecastquery', region_name='us-east-1')
+    forecast_arn='arn:aws:forecast:us-east-1:162690303242:forecast/forecast3_230355121'
+    start_date = request.args.get('start_date')
+    end_date = request.args.get('end_date') 
+    filters = {
+        "item_id" : "MAX"
+    }
+
+    response = client.query_forecast(
+    ForecastArn=forecast_arn,
+    StartDate=start_date,
+    EndDate=end_date,
+    Filters=filters
+    )
+
+    forecast_data = response['Forecast']['Predictions']
+
+    return jsonify(forecast_data)
+
+if __name__=='__main__':
+    app.run()
