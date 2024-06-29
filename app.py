a6d4161e6c4d0a734e40b8845d51db2f42d5b733
MoRan <ooklibaioo@gmail.com>
2024-06-29 20:36:43 +0800

    Forecast

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ app=fla(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/get_data', methods=['GET'])
-def get_data():
+@app.route('/get_max_data', methods=['GET'])
+def get_max_data():
     client = boto3.client('forecastquery', region_name='us-east-1')
     forecast_arn='arn:aws:forecast:us-east-1:162690303242:forecast/forecast3_230355121'
     start_date = request.args.get('start_date')
@@ -29,5 +29,26 @@ def get_data():
 
     return jsonify(forecast_data)
 
+@app.route('/get_min_data', methods=['GET'])
+def get_min_data():
+    client = boto3.client('forecastquery', region_name='us-east-1')
+    forecast_arn='arn:aws:forecast:us-east-1:162690303242:forecast/forecast3_230355121'
+    start_date = request.args.get('start_date')
+    end_date = request.args.get('end_date') 
+    filters = {
+        "item_id" : "MIN"
+    }
+
+    response = client.query_forecast(
+    ForecastArn=forecast_arn,
+    StartDate=start_date,
+    EndDate=end_date,
+    Filters=filters
+    )
+
+    forecast_data = response['Forecast']['Predictions']
+
+    return jsonify(forecast_data)
+
 if __name__=='__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000, debug=True)
